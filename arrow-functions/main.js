6c3613b4c838414a1c4847f3d70dfbe6e7b3cef4
flora <mclaradev@gmail.com>
2024-02-26 15:03:09 -0300

    estrutura js feita e testada.

---
+++ arrow-functions/main.js
@@ -0,0 +1,10 @@
+const arrowSum = (a, b) => {
+    return a + b
+}
+
+console.log(`Soma Arrow Function = ${arrowSum(2, 8)}`)
+
+const arrowSub = (a, b) => a - b
+
+console.log(`Subtração Arrow Function = ${arrowSub(8, 3)}`)
+
