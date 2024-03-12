9d7ffca062a6289d7769e07d0855f484890374f0
flora <mclaradev@gmail.com>
2024-03-12 10:35:21 -0300

    estrutura js completa.

---
+++ encadeamento-opcional/main.js
@@ -0,0 +1,14 @@
+const user = {
+    name: "flora",
+    age: 20,
+    email: "flora@gmail.com",
+    parents:  [{
+        name: "José  Roberto",
+        age: 54,
+        name: "Tatiana",
+        age: 44
+    }]
+}
+
+console.log(user?.age?.adress)
+console.log(user?.age?.email)
