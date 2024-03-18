38cca98d05bc05e4dbd4a19f711404b5a76036dd
flora <mclaradev@gmail.com>
2024-03-18 18:13:29 -0300

    média aritmética ponderada teste

--- exercicio-14/main.js
+++ exercicio-14/main.js
@@ -4,3 +4,16 @@ const average = (...numbers) => {
 }
 
 console.log(`A média aritmética simples: ${average(5, 10, 15, 20)}`)
+
+const weightAverage = (...entries) => {
+    const sum = entries.reduce((accum, {number, weight}) => accum + (number * weight), 0)
+    const weightSum = entries.reduce((accum, entry) => accum + entry.weight, 0)
+    return sum / weightSum
+}
+
+console.log(`A média aritmética ponderada é: ${weightAverage(
+    {number: 5, weight: 3},
+    {number: 10, weight: 4},
+    {number: 15, weight: 5},
+    {number: 20, weight: 6}
+    )}`)
