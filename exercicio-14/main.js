3c13ba0ae0e20df130b928ea4ee79f857ec50a8e
flora <mclaradev@gmail.com>
2024-03-18 18:34:27 -0300

    testando função mediana no js

--- exercicio-14/main.js
+++ exercicio-14/main.js
@@ -21,7 +21,20 @@ console.log(
   `A média aritmética ponderada é: ${weightAverage(
     { number: 5, weight: 3 },
     { number: 10, weight: 4 },
-    { number: 15, weight: 5 },
+    { number: 15 },
     { number: 20, weight: 6 }
   )}`
 );
+
+const median = (...numbers) => {
+    const orderedNumbers = [...numbers].sort((a, b) => a - b)
+    const middle = Math.floor(orderedNumbers.length / 2)
+    if (orderedNumbers.length % 2 !== 0){
+        return orderedNumbers[middle]
+    }
+    const firstMedian = orderedNumbers[middle - 1]
+    const secondeMedian = orderedNumbers[middle]
+    return average(firstMedian, secondeMedian)
+}
+
+console.log(`A função Mediana é: ${median(5, 10, 15, 20, 25)}`)
