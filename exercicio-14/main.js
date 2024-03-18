20c895bd941441ffe88657f0b9c6848db1ad4f89
flora <mclaradev@gmail.com>
2024-03-18 18:22:07 -0300

    formatei o documento

--- exercicio-14/main.js
+++ exercicio-14/main.js
@@ -1,19 +1,27 @@
 const average = (...numbers) => {
-    const sum = numbers.reduce((accum, num) => accum + num, 0)
-    return sum / numbers.length
-}
+  const sum = numbers.reduce((accum, num) => accum + num, 0);
+  return sum / numbers.length;
+};
 
-console.log(`A média aritmética simples: ${average(5, 10, 15, 20)}`)
+console.log(`A média aritmética simples: ${average(5, 10, 15, 20)}`);
 
 const weightAverage = (...entries) => {
-    const sum = entries.reduce((accum, {number, weight}) => accum + (number * weight), 0)
-    const weightSum = entries.reduce((accum, entry) => accum + entry.weight, 0)
-    return sum / weightSum
-}
+  const sum = entries.reduce(
+    (accum, { number, weight }) => accum + number * (weight ?? 1),
+    0
+  );
+  const weightSum = entries.reduce(
+    (accum, entry) => accum + (entry.weight ?? 1),
+    0
+  );
+  return sum / weightSum;
+};
 
-console.log(`A média aritmética ponderada é: ${weightAverage(
-    {number: 5, weight: 3},
-    {number: 10, weight: 4},
-    {number: 15, weight: 5},
-    {number: 20, weight: 6}
-    )}`)
+console.log(
+  `A média aritmética ponderada é: ${weightAverage(
+    { number: 5, weight: 3 },
+    { number: 10, weight: 4 },
+    { number: 15, weight: 5 },
+    { number: 20, weight: 6 }
+  )}`
+);
