4d487ffcc7ee3e8db9cae248f30aa8e2c54b58db
flora <mclaradev@gmail.com>
2024-03-20 14:18:32 -0300

    aprendendo sobre o modulo common js

---
+++ modulos-no-js/common-js/index.js
@@ -0,0 +1,12 @@
+function render() {
+    console.log("Renderizando a Interface da Aplicação...")
+}
+
+function store() {
+    console.log('Salvando as informações do Banco de Dados...')
+}
+
+console.log('Aplicação Iniciada.')
+render()
+store()
+console.log('Aplicação Finalizada.')
