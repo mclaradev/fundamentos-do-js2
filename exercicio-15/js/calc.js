7a8d7745988dfb1e850bfaaee117e160f227da50
flora <mclaradev@gmail.com>
2024-05-20 18:06:31 -0300

    importação do módulo de tema da calculadora

--- exercicio-15/js/calc.js
+++ exercicio-15/js/calc.js
@@ -1,5 +1,6 @@
 import calculate from "./calculate.js";
 import copyToClipBoard from "./copyToClipBoard.js";
+import themeSwitcher from "./themeSwitcher.js";
 
 const main = document.querySelector("main");
 const root = document.querySelector(":root");
@@ -57,18 +58,4 @@ document.getElementById("equal").addEventListener("click", calculate);
 
 document.getElementById("copyToClipboard").addEventListener("click", copyToClipBoard)
 
-document.getElementById("themeSwitcher").addEventListener("click", function () {
-  if (main.dataset.theme === "dark") {
-    root.style.setProperty("--bg-color", "#f1f5f9");
-    root.style.setProperty("--border-color", "#aaa");
-    root.style.setProperty("--font-color", "#212529");
-    root.style.setProperty("--primary-color", "#26834a");
-    main.dataset.theme = "light";
-  } else {
-    root.style.setProperty("--bg-color", "#212529");
-    root.style.setProperty("--border-color", "#666");
-    root.style.setProperty("--font-color", "#f1f5f9");
-    root.style.setProperty("--primary-color", "#4dff91");
-    main.dataset.theme = "dark";
-  }
-});
+document.getElementById("themeSwitcher").addEventListener("click", themeSwitcher)
