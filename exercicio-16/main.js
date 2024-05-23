ba4446687e5746dad1dee50d73507a2766782812
flora <mclaradev@gmail.com>
2024-05-22 21:02:07 -0300

    função para "calcular" a idade.

--- exercicio-16/main.js
+++ exercicio-16/main.js
@@ -1,1 +1,12 @@
 const dayjs = require("dayjs");
+
+function birthday(date) {
+    const birthday = dayjs(date)
+    const today = dayjs()
+
+    const ageInYears = today.diff(birthday, 'year')
+
+    console.log(`Você tem: ${ageInYears} anos de idade.`)
+}
+
+birthday("2003-10-30")
