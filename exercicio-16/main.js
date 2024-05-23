19105abb5417e658ea13b96ca2194cf53eeda61c
flora <mclaradev@gmail.com>
2024-05-22 21:26:24 -0300

    código para calcular quantos dias faltam para o próximo aniverário

--- exercicio-16/main.js
+++ exercicio-16/main.js
@@ -6,9 +6,11 @@ function birthday(date) {
 
     const ageInYears = today.diff(birthday, 'year')
     const nextBirthday = birthday.add(ageInYears + 1, "year")
+    const dayToNextBirthday = nextBirthday.diff(today, 'day') + 1
 
     console.log(`Você tem: ${ageInYears} anos de idade.`)
     console.log(`Próximo aniversário: ${nextBirthday.format("DD/MM/YYYY")}`)
+    console.log(`Faltam ${dayToNextBirthday} dias para você completar ${ageInYears + 1} anos de idade.`)
 }
 
 birthday("2003-10-30")
