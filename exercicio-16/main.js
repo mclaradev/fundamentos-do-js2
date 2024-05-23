ff769f91e88e081aa460242e956db8057d1c53ef
flora <mclaradev@gmail.com>
2024-05-22 21:06:09 -0300

    código para mostrar a data de aniversário

--- exercicio-16/main.js
+++ exercicio-16/main.js
@@ -5,8 +5,10 @@ function birthday(date) {
     const today = dayjs()
 
     const ageInYears = today.diff(birthday, 'year')
+    const nextBirthday = birthday.add(ageInYears + 1, "year")
 
     console.log(`Você tem: ${ageInYears} anos de idade.`)
+    console.log(`Próximo aniversário: ${nextBirthday.format("DD/MM/YYYY")}`)
 }
 
 birthday("2003-10-30")
