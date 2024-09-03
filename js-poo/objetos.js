35ad7d792e665c1b31119f6d3ef24976bee0e53f
flora <mclaradev@gmail.com>
2024-09-03 06:21:51 -0300

    testando alguns comandos básicos do objeto

--- js-poo/objetos.js
+++ js-poo/objetos.js
@@ -5,6 +5,12 @@ const book = {
     pagers: 217,
     published: true,
     inStock: 300,
-    tags: ["Ficção Juvenil", "Humor"]
+    tags: ["Ficção Juvenil", "Humor"],
+    author: {
+        name: "Jeff Kinney"
+    }
 }
 
+console.log(book)
+
+console.log(book.pagers)
