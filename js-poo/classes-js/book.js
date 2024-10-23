d30f7abd993c64f5c7be81647d81d68c86b396cc
flora <mclaradev@gmail.com>
2024-10-23 18:43:21 -0300

    não lembro pois já faz um tempo que não mexo aqui

---
+++ js-poo/classes-js/book.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(title) {
+        this.title = title
+        this.published = false
+    }
+}
+
+const diario = new Book("Diário de um Banana")
+
+console.log(diario)
